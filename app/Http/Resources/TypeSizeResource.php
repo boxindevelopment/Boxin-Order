9617b7a58c6e8e32e58af44f208dd879f8306d19
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-08-23 11:17:49 +0700

    image url

--- app/Http/Resources/TypeSizeResource.php
+++ app/Http/Resources/TypeSizeResource.php
@@ -14,12 +14,13 @@ class TypeSizeResource extends JsonResource
      */
     public function toArray($request)
     {
+        $url = 'https://boxin-dev-webbackend.azurewebsites.net/';
 
         $data = [
             'id'                => $this->id,
             'name'              => $this->name,
             'size'              => $this->size,
-            'image'             => is_null($this->image) ? null : (asset('images/types_of_size').'/'.$this->image),
+            'image'             => is_null($this->image) ? null : $url.'images/types_of_size'.'/'.$this->image,
             'types_of_box_room' => new TypeBoxRoomResource($this->type_box_room),
         ];
 
