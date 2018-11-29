1f0ed30a87dd3903be5d9dc37a5b621104cfc552
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-11-29 17:37:38 +0700

    Create CityResource
    - response API city

--- app/Http/Resources/CityResource.php
+++ app/Http/Resources/CityResource.php
@@ -4,14 +4,13 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
 
-class AreaResource extends Resource
+class CityResource extends Resource
 {
     public function toArray($request)
     {
         return [
             'id'            => $this->id,
-            'name'          => $this->name,
-            'city'          => new CityResource($this->city),  
+            'name'          => $this->name, 
         ];
     }
 }
