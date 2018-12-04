05ed58d10958ae3081f3648fdb49d53e670112b0
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-12-04 11:04:26 +0700

    Create model/table setting
    table setting

---
+++ app/Model/Setting.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+
+    protected $table = 'settings';
+
+    protected $fillable = [
+        'name', 'value', 'unit', 'description',
+    ];
+
+    protected $searchable = ['id', 'name'];
+    
+}
