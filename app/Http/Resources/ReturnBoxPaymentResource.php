2e34c22b126536bd104a446f1c6017db22f620ad
Mei Dina Isti Nurmala <meidinaistinurmala@gmail.com>
2018-12-11 16:58:10 +0700

    benerin api paymnet return (3)

--- app/Http/Resources/ReturnBoxPaymentResource.php
+++ app/Http/Resources/ReturnBoxPaymentResource.php
@@ -18,7 +18,7 @@ class ReturnBoxPaymentResource extends JsonResource
         $data = [
             'id'                => $this->id,
             'id_name'           => $this->id_name,
-            'order_detail_id'   => new OrderDetail($this->order_detail),
+            'order_detail_id'   => new OrderDetailResource($this->order_detail),
             'payment_date'      => $this->created_at->format('Y-m-d H:i:s'), 
             'payment_type'      => $this->payment_type, 
             'bank'              => $this->bank, 
