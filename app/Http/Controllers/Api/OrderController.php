40cb133366e22bdb81736b6d5257414be3c7c17e
Wawan Randianto <wawanrandianto1@gmail.com>
2019-04-24 17:16:12 +0700

    fix bug

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -554,11 +554,10 @@ class OrderController extends Controller
       $date_return        = Carbon::now()->toDateString();
       
       $date_now      = Carbon::now()->toDateTimeString();
-      $order_details = OrderDetail::whereDate('end_date', '>=', $date_now)->get();
+      $order_details = OrderDetail::whereDate('end_date', '<', $date_now)->get();
       // $array_id_order_detail = OrderDetail::whereDate('end_date', '>=', $date_now)->pluck('id')->toArray();
       // $query_id_order = OrderDetail::selectRaw('DISTINCT(order_id) as order_id')->whereDate('end_date', '>=', $date_now)->pluck('order_id')->toArray();
       // $array_id_order = array_map('intval', $query_id_order);
-      dd($order_details);
       DB::beginTransaction();
       try {
 
