87c2b42823289f836b5c8230a96e1788f4b727fc
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-07-23 09:07:21 +0700

    Log info

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -410,22 +410,15 @@ class PaymentController extends Controller
 
     public function callbackNotif(Request $request)
     {
-        Log::info("Midtrans Notication");
       $midtrans = new Vtdirect();
       $json_result = file_get_contents('php://input');
       $result = json_decode($json_result);
 
       $notif = null;
       if ($result) {
-        Log::info("Order ID : " . $result->order_id);
         $notif = $midtrans->checkStatus($result->order_id);
-        Log::info(print_r($notif, true));
       }
 
-
-      Log::info("Order status : " . $notif['transaction_status']);
-      Log::info("Order Type : " . $notif['payment_type']);
-      Log::info("Order Fraun : " . $notif['fraud_status']);
       $transaction = $notif['transaction_status'];
       $type        = $notif['payment_type'];
       $order_id    = $notif['order_id'];
