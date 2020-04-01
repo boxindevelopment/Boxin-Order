1476632ad8f8a23c7a19463c5d4afd4a629e60a9
yonathanYPP <yonathan.ypp@gmail.com>
2020-04-01 14:02:31 +0700

    add function getbyid

--- routes/api.php
+++ routes/api.php
@@ -129,6 +129,7 @@ Route::group(['namespace' => 'Api'], function() {
 
     Route::prefix('promotion')->group(function() {
         Route::get('voucher/list', 'VoucherController@index')->name('api.voucher.index');
+        Route::get('voucherByid/{id}', 'VoucherController@FindOne')->name('api.voucher.index');
         Route::post('voucher/{voucher_id}', 'VoucherController@detail')->name('api.voucher.detail');
         Route::get('banner/list', 'BannerController@index')->name('api.banner.index');
     });
