e9a7b32542b9876b156c193d5046e19463521438
Bahtiar <bahtiar2008.uchiha@gmail.com>
2020-11-20 16:41:42 +0700

    PriceResource

--- app/Http/Controllers/Api/SpaceController.php
+++ app/Http/Controllers/Api/SpaceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SpaceSmallResource;
+use App\Http\Resources\PriceResource;
 use App\Repositories\Contracts\SpaceSmallRepository;
 
 class SpaceController extends Controller
